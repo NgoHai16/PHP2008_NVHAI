9545182aab37c7c5b2c34d889e6075b7a488010c
NgoHai16 <Hainv720@wru.vn>
2020-11-23 19:53:44 +0700

    Add new index file

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+    <form action="giaipt.php" method="POST">
+        Nhập a : <input type="text" name="a"><br/>
+        Nhập b : <input type="text" name="b"><br/>
+        Nhập c : <input type="text" name="c"><br/>
+        <input type="submit">
+    </form>
+    
+</body>
+</html>
